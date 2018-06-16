8647aee5bcee1d525dd7024bdf11778c2c11123a
jarekgm <31661966+jarekgm@users.noreply.github.com>
2018-06-16 22:08:01 +0200

    Add files via upload

---
+++ .gitignore/index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120895187-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120895187-1');
+</script>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="Keywords" content="Roztocze, Zwierzyniec, Obrocz, noclegi, domek do wynajęcia, urlop, spływy kajakowe, Roztoczański Park Narodowy" />
+  <meta name="Description" content="Domek położony jest w samym centrum Roztoczańskiego Parku Narodowego w miejscowości Obrocz. Wioska jest pięknie położona i otoczona z każdej strony lasem iglastym co stanowi wspaniały mikroklimat. Jest to idealne miejsce do relaksu na łonie natury
+    z dala od zgiełku dużych miast i od codziennych obowiązków." />
+  <title>Domek u Gmyza, Domek na Roztoczu, Nocleg Zwierzyniec, Spływy kajakowe, Urlop</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/lightbox.css">
+  <link rel="stylesheet" href="css/queries.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+  <link href="https://unpkg.com/ionicons@4.1.2/dist/css/ionicons.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+<link rel="apple-touch-icon" sizes="180x180" href="/img/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicons/favicon-16x16.png">
+<link rel="manifest" href="/img/favicons/site.webmanifest">
+<link rel="mask-icon" href="/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="/img/favicons/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
+</head>
+
+<body>
+  <header id="stronaGlowna" class="stronaGlowna">
+    <a class="mobile-navi-icon js--nav-icon"><i class="fas fa-bars"></i> </a>
+    <nav>
+      <div class="main-nav js--main-nav">
+          <ul>
+            <li class="active"><a href="#stronaGlowna">Strona główna</a></li>
+            <li><a href="#domek">Domek</a></li>
+            <li><a href="#atrakcje">Atrakcje</a></li>
+            <li><a href="#cennik">Cennik</a></li>
+            <li><a href="#kontakt">Kontakt</a></li>
+          </ul>
+        </div>
+    </nav>
+
+    <div class="logo">
+      <h1>Domek u Gmyza</h1>
+    </div>
+
+    <div class="btn-main">
+      <a class="btn" href="#domek">Zapraszamy</a>
+    </div>
+    <div class="btn btn-main phone">
+      <a href="tel:+48519755086"> <p><i class="fas fa-phone footer-icon"></i><br>519755086<br></p></a>
+  </div>
+
+  </header>
+
+  <section id="domek" class="js--oferta">
+    <h2 class="naglowek">Nasz domek</h2>
+    <div class="oferta-opis row">
+
+        <div>
+          <img src="img/domek/domek.jpg" alt="Domek nocą">
+        </div>
+
+      <div class="btn-srodek">
+        <a class="btn btn-srodek-a" href="#srodek">Zobacz środek</a>
+      </div>
+    </div>
+
+    <div id="srodek" class="srodek">
+      <div class="row">
+        <h3 class="naglowek">Salon</h3>
+    </div>
+    <div class="row">
+      <div class="flex">
+        <div class="divOferta">
+              <a href="img/domek/salon1.JPG" data-lightbox="domek"> <img src="img/domek/salon1.JPG" alt="Salon1"></a>
+        </div>
+        <div class="divOferta">
+            <a href="img/domek/salon2.JPG" data-lightbox="domek"> <img src="img/domek/salon2.JPG" alt="Salon2"></a>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <div class="row">
+      <h3 class="naglowek">Aneks kuchenny</h3>
+    </div>
+    <div class="row">
+      <div class="flex">
+        <div class="divOferta">
+            <a href="img/domek/aneks.JPG" data-lightbox="domek"> <img src="img/domek/aneks.JPG" alt="aneks kuchenny"></a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <h3 class="naglowek">Łazienka</h3>
+    </div>
+    <div class="row">
+      <div class="flex">
+        <div class="divOferta">
+            <a href="img/domek/lazienka1.JPG" data-lightbox="domek"> <img src="img/domek/lazienka1.JPG" alt="Łazienka1"></a>
+        </div>
+        <div class="divOferta">
+            <a href="img/domek/lazienka2.JPG" data-lightbox="domek"> <img src="img/domek/lazienka2.JPG" alt="Łazienka2"></a>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <h3 class="naglowek">Taras</h3>
+    </div>
+    <div class="row">
+      <div class="flex">
+        <div class="divOferta">
+            <a href="img/domek/taras1.JPG" data-lightbox="domek"> <img src="img/domek/taras1.JPG" alt="Taras1"></a>
+        </div>
+        <div class="divOferta">
+            <a href="img/domek/taras2.jpg" data-lightbox="domek"> <img src="img/domek/taras2.jpg" alt="Taras2"></a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="sekcja-zdjecia" id="atrakcje">
+    <ul class="zdjecia-okolica clearfix">
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/1.jpg" data-lightbox="roadtrip"> <img src="img/sekcja-zdjecia/1-min.jpg" alt="florianka"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/2.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/2-min.jpg" alt="rowery przy stawach echo"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/3.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/3-min.jpg" alt="stawy echo"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/4.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/4-min.jpg" alt="most na Zwierzyńczyku"></a>
+        </figure>
+      </li>
+    </ul>
+    <ul class="zdjecia-okolica clearfix">
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/5.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/5-min.jpg" alt="florianka"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/6.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/6-min.jpg" alt="rowery przy stawach echo"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/7.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/7-min.jpg" alt="rowery przy stawach echo"></a>
+        </figure>
+      </li>
+      <li>
+        <figure class="zdjecia-okolica_zdjecie">
+          <a href="img/sekcja-zdjecia/8.jpg" data-lightbox="roadtrip"><img src="img/sekcja-zdjecia/8-min.jpg" alt="most na Zwierzyńczyku"></a>
+        </figure>
+      </li>
+    </ul>
+  </section>
+
+  <section id="cennik" class="cennik">
+    <div class="row">
+      <h2><i class="icon fas fa-hand-holding-usd"></i><br>cennik</h2>
+
+    </div>
+    <div class="row">
+      <div class="col span-1-of-2">
+        <span>2 osoby</span>
+      </div>
+      <div class="col span-1-of-2">
+        <span>140zł</span>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col span-1-of-2">
+        <span>3 osoby</span>
+      </div>
+      <div class="col span-1-of-2">
+        <span>160zł</span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col span-1-of-2">
+        <span>4 osoby</span>
+      </div>
+      <div class="col span-1-of-2">
+        <span>180zł</span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col span-1-of-2">
+        <span>5 osób</span>
+      </div>
+      <div class="col span-1-of-2">
+        <span>200zł</span>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col span-1-of-2">
+        <span>6 osób</span>
+      </div>
+      <div class="col span-1-of-2">
+        <span>240zł</span>
+      </div>
+      </div>
+      <div class="row">
+        <div class="negocjacja">* przy większych grupach i dłuższych pobytach, możliwa negocjacja cen</div>
+        <div class="negocjacja">* w celu potwierdzenia rezerwacji prosimy o wpłatę 20% ustalonej kwoty na konto</div>
+      <div class="row">
+        <div class="wyposazenie"> <h4>wyposażenie domku</h4>zestaw naczyń i garnków, komplet kieliszków i szklanek, lodówka, tv, kuchnia indukcyjna, czajnik elektryczny, kabina prysznicowa z radiem, Internet, grill</div>
+      </div>
+      </div>
+  </section>
+
+  <div class="map-container">
+    <div id="map"></div>
+    <form class="contact-form" action="mailer-new.php" method="post" id="kontakt">
+      <div class="row">
+        <h2>Zapytaj o wolne terminy</h2>
+      </div>
+      <div class="row">
+        <?php
+          if($_GET['success'] == 1){
+            echo "<div class=\"form-message success\">Dziękujemy za wysłanie wiadomości! Odpowiemy możliwie szybko.</div>";
+          }
+          else{
+            echo "<div class=\"form-message\"></div>";
+          }
+
+          if($_GET['success'] == -1){
+            echo "<div class=\"form-message error\">Ojoj! Coś poszło nie tak. Spróbuj ponownie.</div>";
+          }
+        ?>
+      </div>
+      <div class="form">
+        <div class="row">
+          <div class="col span-5-of-12">
+            <label for="imie">Imię</label>
+          </div>
+          <div class="col span-7-of-12">
+            <input type="text" name="imie" placeholder="Imię" id="imie" required>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col span-5-of-12">
+            <label for="email">Email</label>
+          </div>
+          <div class="col span-7-of-12">
+            <input type="text" name="email" placeholder="Email" id="email" required>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col span-5-of-12">
+            <label for="wiadomosc">Wiadomość</label>
+          </div>
+          <div class="col span-7-of-12">
+            <textarea name="wiadomosc" placeholder="Wiadomość" id="wiadomosc" required></textarea>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col span-5-of-12">
+            <label>&nbsp;</label>
+          </div>
+          <div class="col span-7-of-12">
+            <input class="btn" type="submit" value="Wyślij">
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+
+<footer>
+  <div class="row">
+      <div class="col span-1-of-4 footer-icon">
+        <p><i class="fas fa-home"></i><br> Grzegorz Gmyz <br> Obrocz 109 <br>22-470 Zwierzyniec<br></p>
+    </div>
+    <div class="col span-1-of-4">
+        <a href="tel:+48519755086"> <p><i class="fas fa-phone footer-icon"></i><br>519755086<br></p></a>
+  </div>
+    <div class="col span-1-of-4 footer-icon">
+      <p><i class="fas fa-at"></i> <br> grzegorz.gmyz@gmail.com</p>
+  </div>
+    <div class="col span-1-of-4 footer-icon">
+        <p><i class="fas fa-university"></i><br>Konto bankowe<br> 52 1140 2004 0000 3502 6201 8492 </p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="copyright">
+      Wszelkie prawa zastrzeżone &copy; 2018 Zdjęcia dzięki uprzejmości MiG Zwierzyniec
+    </div>
+
+  </div>
+</footer>
+
+  <script>
+// Initialize and add the map
+function initMap() {
+// The location of Uluru
+var obrocz = {lat: 50.602607, lng: 23.027244};
+var center = {lat: 50.602607, lng: 23.15}
+// The map, centered at Uluru
+var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 11, center: center});
+// The marker, positioned at Uluru
+var marker = new google.maps.Marker({position: obrocz, map: map});
+}
+  </script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA32JUdf6MgS4be4DbnfTj2rLbsmxc3pEg&callback=initMap"></script>
+  <script src="https://code.jquery.com/jquery-2.2.4.js " integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin=""></script>
+  <script src="js/script.js"></script>
+  <script src="js/lightbox.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="https://unpkg.com/ionicons@4.1.2/dist/ionicons.js"></script>
+</body>
+
+</html>
